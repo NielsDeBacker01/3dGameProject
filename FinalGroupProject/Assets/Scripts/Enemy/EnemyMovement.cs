2353481b344037007b6dd9befe1cc33b5ffe2431
BlackMagicianX <benalimarwanmbc@gmail.com>
2023-01-18 12:05:38 +0100

    Improve Enemy Movement

--- FinalGroupProject/Assets/Scripts/Enemy/EnemyMovement.cs
+++ FinalGroupProject/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -12,6 +12,10 @@ public class EnemyMovement : MonoBehaviour
     public float multiplier = 10;
     public float timer = 0f;
     public float delay;
+    public float chargeTime;
+
+    public float idleTurnSpeed = 10;
+    public float chargeTurnSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,15 +29,18 @@ public class EnemyMovement : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer >= delay && timer < (delay + 1)){
+        if (timer >= delay && timer < (delay + chargeTime/1.75)){
             transform.position += transform.forward * speed * multiplier * Time.deltaTime;
-        } else if (timer >= (delay + 1)){
+        } else if(timer >= delay && timer > (delay + chargeTime/3) && timer < (delay + chargeTime)){
+            SmoothLookAtPlayer(chargeTurnSpeed);
+            transform.position += transform.forward * speed * multiplier/2 * Time.deltaTime;
+        }else if (timer >= (delay + 1)){
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
             timer = 0f;
         } else {
         // Smoothly rotate towards Player
-        SmoothLookAtPlayer();
+        SmoothLookAtPlayer(idleTurnSpeed);
         }
     }
 
@@ -44,10 +51,10 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-    void SmoothLookAtPlayer(){
+    void SmoothLookAtPlayer(float turnspeed){
         Vector3 dir = player.transform.position - transform.position;
         Quaternion rot = Quaternion.LookRotation(dir);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, turnspeed * Time.deltaTime);
         transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
